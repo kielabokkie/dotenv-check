c4a0e9b73685f14b565e560a6486b1f922f8f1dd
kielabokkie <kielabokkie@gmail.com>
2015-08-09 22:26:06 +1200

    Tidied up the code a bit

--- src/DotenvCheck.php
+++ src/DotenvCheck.php
@@ -5,34 +5,16 @@ use Symfony\Component\Console\Output\ConsoleOutput;
 
 class DotenvCheck
 {
+    /**
+     * Run the dotenv checker
+     */
     public static function run()
     {
         $output = new ConsoleOutput;
         $formatter = new FormatterHelper;
 
-        $file = fopen(".env", "r");
-        $env = [];
-        while (feof($file) === false) {
-            $line = fgets($file);
-            $lineAry = explode("=", $line);
-            $key = trim($lineAry[0]);
-            if (empty($key) === false) {
-                $value = trim($lineAry[1]);
-                $env[$key] = $value;
-            }
-        }
-
-        $file = fopen(".env.example", "r");
-        $envExample = [];
-        while (feof($file) === false) {
-            $line = fgets($file);
-            $lineAry = explode("=", $line);
-            $key = trim($lineAry[0]);
-            if (empty($key) === false) {
-                $value = trim($lineAry[1]);
-                $envExample[$key] = $value;
-            }
-        }
+        $env = self::readFile('.env');
+        $envExample = self::readFile('.env.example');
 
         // Check which variables are missing and which ones are extra
         $envKeys = array_keys($env);
@@ -47,19 +29,45 @@ class DotenvCheck
         }
 
         if (count($missing) > 0) {
-            $output->writeln("\n<comment>The following variables are missing from your .env file<comment>");
+            $output->writeln("\n<comment>The following variables are missing from your .env file:<comment>");
             foreach ($missing as $variable) {
                 $output->writeln(sprintf('<info>%s=%s<info>', $variable, $envExample[$variable]));
             }
         }
 
         if (count($extra) > 0) {
-            $output->writeln("\n<comment>The following variables are in your .env file but not in .env.example<comment>");
+            $output->writeln("\n<comment>The following variables are in your .env file but not in .env.example:<comment>");
             foreach ($extra as $variable) {
                 $output->writeln(sprintf('<info>%s=%s<info>', $variable, $env[$variable]));
             }
         }
 
         $output->writeln('');
+    }
+
+    /**
+     * Read the file and create an array of the key value pairs
+     *
+     * @param  string $fileName Name of the file to be read
+     * @return array            Array of variable names and their value
+     */
+    private static function readFile($fileName)
+    {
+        $file = fopen($fileName, 'r');
+
+        $result = [];
+        while (feof($file) === false) {
+            $line = trim(fgets($file));
+
+            // Ignore empty lines
+            if (empty($line) === true) {
+                continue;
+            }
+
+            $lineArray = explode("=", $line);
+            $result[$lineArray[0]] = $lineArray[1];
+        }
+
+        return $result;
     }
 }
