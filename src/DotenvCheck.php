aeaf965e964e0ec921fc5a3a500d51f32e4c37ea
kielabokkie <kielabokkie@gmail.com>
2015-08-09 22:11:37 +1200

    Show variable name and value in list of missing environment variables

--- src/DotenvCheck.php
+++ src/DotenvCheck.php
@@ -13,46 +13,50 @@ class DotenvCheck
         $file = fopen(".env", "r");
         $env = [];
         while (feof($file) === false) {
-            $line    = fgets($file);
+            $line = fgets($file);
             $lineAry = explode("=", $line);
             $key = trim($lineAry[0]);
             if (empty($key) === false) {
-                $env[] = $key;
+                $value = trim($lineAry[1]);
+                $env[$key] = $value;
             }
         }
 
         $file = fopen(".env.example", "r");
         $envExample = [];
         while (feof($file) === false) {
-            $line    = fgets($file);
+            $line = fgets($file);
             $lineAry = explode("=", $line);
             $key = trim($lineAry[0]);
             if (empty($key) === false) {
-                $envExample[] = $key;
+                $value = trim($lineAry[1]);
+                $envExample[$key] = $value;
             }
         }
 
         // Check which variables are missing and which ones are extra
-        $missing = array_diff($envExample, $env);
-        $extra = array_diff($env, $envExample);
+        $envKeys = array_keys($env);
+        $envExampleKeys = array_keys($envExample);
+        $missing = array_diff($envExampleKeys, $envKeys);
+        $extra = array_diff($envKeys, $envExampleKeys);
 
         if (count($missing) > 0 || count($extra) > 0) {
             $output->writeln('');
-            $warning = $formatter->formatBlock('Warning: Your .env and .env.example are not in sync.', 'bg=blue;fg=white', true);
+            $warning = $formatter->formatBlock('Warning: Your .env and .env.example files are not in sync.', 'bg=blue;fg=white', true);
             $output->writeln($warning);
         }
 
         if (count($missing) > 0) {
             $output->writeln("\n<comment>The following variables are missing from your .env file<comment>");
             foreach ($missing as $variable) {
-                $output->writeln(sprintf('<info>- %s<info>', $variable));
+                $output->writeln(sprintf('<info>%s=%s<info>', $variable, $envExample[$variable]));
             }
         }
 
         if (count($extra) > 0) {
             $output->writeln("\n<comment>The following variables are in your .env file but not in .env.example<comment>");
             foreach ($extra as $variable) {
-                $output->writeln(sprintf('<info>- %s<info>', $variable));
+                $output->writeln(sprintf('<info>%s=%s<info>', $variable, $env[$variable]));
             }
         }
 
