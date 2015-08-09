bdf4c868bfdbbcbf6db5deb126ee5c8d627a2dce
kielabokkie <kielabokkie@gmail.com>
2015-08-09 21:37:49 +1200

    Use Symfony console command instead

--- src/DotenvCheck.php
+++ src/DotenvCheck.php
@@ -1,12 +1,14 @@
-<?php namespace Kielabokkie\DotenvCheck;
+<?php namespace Kielabokkie;
 
-use League\CLImate\CLImate;
+use Symfony\Component\Console\Helper\FormatterHelper;
+use Symfony\Component\Console\Output\ConsoleOutput;
 
 class DotenvCheck
 {
-    public static function check()
+    public static function run()
     {
-        $cli = new CLImate;
+        $output = new ConsoleOutput;
+        $formatter = new FormatterHelper;
 
         $file = fopen(".env", "r");
         $env = [];
@@ -30,16 +32,30 @@ class DotenvCheck
             }
         }
 
-        $cli->br()->yellow('The following variables are missing from your .env file');
-        $result = array_diff($envExample, $env);
-        foreach ($result as $variable) {
-            $cli->red(sprintf('- %s', $variable));
+        // Check which variables are missing and which ones are extra
+        $missing = array_diff($envExample, $env);
+        $extra = array_diff($env, $envExample);
+
+        if (count($missing) > 0 || count($extra) > 0) {
+            $output->writeln('');
+            $warning = $formatter->formatBlock('Warning: Your .env and .env.example are not in sync.', 'bg=blue;fg=white', true);
+            $output->writeln($warning);
         }
 
-        $cli->br()->yellow('The following variables are in your .env file but not in .env.example');
-        $result = array_diff($env, $envExample);
-        foreach ($result as $variable) {
-            $cli->red(sprintf('- %s', $variable));
+        if (count($missing) > 0) {
+            $output->writeln("\n<comment>The following variables are missing from your .env file<comment>");
+            foreach ($missing as $variable) {
+                $output->writeln(sprintf('<info>- %s<info>', $variable));
+            }
         }
+
+        if (count($extra) > 0) {
+            $output->writeln("\n<comment>The following variables are in your .env file but not in .env.example<comment>");
+            foreach ($extra as $variable) {
+                $output->writeln(sprintf('<info>- %s<info>', $variable));
+            }
+        }
+
+        $output->writeln('');
     }
 }
